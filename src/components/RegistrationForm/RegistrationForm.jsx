c96e1f501cf0010de0f8632f69754e94d723ae58
Edgaras Austinskas <eaustinskas@gmail.com>
2023-12-11 23:01:08 +0200

    edited

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -4,14 +4,14 @@ import './RegistrationForm.scss';
 
 const RegistrationForm = () => {
   const handleFormSubmit = (formData) => {
-    // Handle form submission logic here
+
     console.log('Form submitted:', formData);
   };
 
   return (
     <div className="registration-form">
-      {/* Add images and logo here */}
-      {/* Example: <img src="logo.png" alt="Logo" /> */}
+      {}
+      {}
       <Form onSubmit={handleFormSubmit} />
     </div>
   );
